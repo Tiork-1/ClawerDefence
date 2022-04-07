0008ecf1dbac50661d7aca31f85ab3b05e9416d8
Qiuxinhan <120975969@qq.com>
2022-04-07 11:32:35 +0800

    I am tired

--- src/main/java/com/example/clawerdefence/controller/BlogController.java
+++ src/main/java/com/example/clawerdefence/controller/BlogController.java
@@ -6,9 +6,11 @@ import com.example.clawerdefence.service.PassageServece;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class BlogController {
@@ -19,8 +21,20 @@ public class BlogController {
     @GetMapping("/getbloglist")
     @ResponseBody
     public String getBlogList(){
-        System.out.println("1111111111111111");
         List<Passage> passageList = passageServece.getAllPassages();
         return JSON.toJSONString(passageList);
+    }
+
+    @GetMapping("/blogEditor")
+    public String blogEditor(){
+        return "/html/addblog.html";
+    }
+
+    @GetMapping("/addblog")
+    public String addBlog(@RequestBody Map<String,Object> args){
+        String title = (String) args.get("title");
+        String context = (String) args.get("context");
+        passageServece.addPassage(new Passage(null,title,context));
+        return "forward:/blogs";
     }
 }
--- src/main/java/com/example/clawerdefence/service/ShowBlogController.java
+++ src/main/java/com/example/clawerdefence/service/ShowBlogController.java
@@ -1,18 +1,45 @@
 package com.example.clawerdefence.service;
 
 
+import com.alibaba.fastjson.JSON;
+import com.example.clawerdefence.pojo.Passage;
+import com.example.clawerdefence.pojo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/blogs")
 public class ShowBlogController {
 
+    @Autowired
+    PassageServece passageServece;
+
     @RequestMapping("/{id}")
-    public String getSpecificPassage(Model model,String id){
-        model.addAttribute("id",id);
+    public String getSpecificPassage(HttpSession session, @PathVariable("id") String id){
+        session.setAttribute("blogId",id);
         return "/html/showblog.html";
+    }
+
+    @RequestMapping("/getReadingBlog")
+    @ResponseBody
+    public String getReadingBlog(HttpSession session) {
+        String id = (String) session.getAttribute("blogId");
+        Result result = new Result(true,""+id);
+        return JSON.toJSONString(result);
+    }
+
+    @RequestMapping("/getBlogById")
+    @ResponseBody
+    public String getBlogById(@RequestBody Map<String,Object> args){
+        String id = (String) args.get("id");
+        System.out.println(id);
+        Passage passage = passageServece.getPassageById(id);
+        System.out.println(passage);
+        return JSON.toJSONString(passage);
     }
 }
